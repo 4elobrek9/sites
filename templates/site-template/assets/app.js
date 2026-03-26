d7cbf6324d9d92e27c70db67c5b72f18f724de7c
4elobrek9 <abobys7228@gmail.com>
2026-03-26 13:25:47 +0300

    fix

--- templates/site-template/assets/app.js
+++
@@ -1,6 +0,0 @@
-const btn = document.getElementById('actionBtn');
-const message = document.getElementById('message');
-
-btn?.addEventListener('click', () => {
-  message.textContent = 'Шаблон работает. Замените этот текст под свою тему.';
-});
